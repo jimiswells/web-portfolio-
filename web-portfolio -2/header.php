379c71137e913aff46726f88ebf2427700a09fcb
“jim <jimocmorrison@googlemail.com>
2014-09-29 16:03:06 +0100

    initisl commit

---
+++ web-portfolio -2/header.php
@@ -0,0 +1,23 @@
+    <header>
+    
+    <div class="row header-content">
+        <div class="large-3 small-12 columns medium-6 medium-centered large-uncentered">
+          <a href="index.html"></a><h2 class="logo">CRECHE</h2></a>
+        </div>
+    
+<nav>
+    
+    
+    <div class="large-5 columns large-offset-4 large-uncentered medium-8 medium-centered  small-12 ">
+       <ul class="navigation">
+          
+           <li class="home"> <a href="#" data-reveal-id="food-menu-modal">menu</a></li>
+           <li class="home"><a href="#" data-reveal-id="wine-menu-modal">wine menu</a></li>
+           <li class="home"><a href="#about-us-anchor">about</a></li>
+           <li class="home"><a href="#contact-us-anchor">contact</a></li>
+       </ul>
+    
+ </div>
+ </div>
+ </nav>
+</header>  
